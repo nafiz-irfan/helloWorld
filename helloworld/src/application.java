911a9bec3e161370d7b017c3ee6f7e80533b2946
nafiz-irfan <nafiz.ace@gmail.com>
2019-09-02 22:28:30 +0800

    add application class

---
+++ helloworld/src/application.java
@@ -0,0 +1,24 @@
+public class application  {
+    public static void main(String[] args) {
+        int myNumber = 1994;
+        short myShort = 837;
+        long myLong = 31094;
+
+        double myDouble = 7.3243;
+        float myFloat = 324.1f;
+
+        char myChar = 'Y';
+        boolean myBoolean = false;
+
+        byte myByte = 127;
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
