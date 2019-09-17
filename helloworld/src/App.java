f3e0af7a4f2a62c564fa658c520545dc1a036739
nafiz-irfan <nafiz.ace@gmail.com>
2019-09-17 23:26:25 +0800

    Static and Final

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,30 +1,46 @@
-class Machine{
-    private String name;
-    private int code;
+class Thing{
+    public final static int LUCKY_NUMBER = 7;
 
-    public Machine(){
-//        this("Arnie", 0);
-        System.out.println("Constructor running!");
+    public String name;
+    public static String description;
+
+    public static int count = 0;
+
+    public int id;
+
+    public Thing(){
+        id = count;
+        count++;
     }
 
-    public Machine(String name){
-        this(name, 0);
-        System.out.println("Second constructor running!");
-//        this.name = name;
+    public void showName(){
+        System.out.println("Object id: " + id + ", " + description + ": " + name);
     }
 
-    public Machine(String name, int code){
-        this();
-        System.out.println("Third constructor running!");
-//        this.name();
-//        this.code();
+    public static void showInfo(){
+        System.out.println(description);
+        //Won't work: System.out.println(name);
     }
 }
 public class App {
     public static void main(String [] args){
-        Machine machine1 = new Machine();
-        Machine machine2 = new Machine("Bertie");
-        Machine machine3 = new Machine("Ahmad", 10);
+       Thing.description = "I am a thing";
+
+       Thing.showInfo();
+
+       Thing thing1 = new Thing();
+       Thing thing2 = new Thing();
+
+       System.out.println("After creating objects, count is: " + Thing.count);
+
+       thing1.name = "Bob";
+       thing2.name = "Sue";
+
+       thing1.showName();
+       thing2.showName();
+
+       System.out.println(Math.PI);
+       System.out.println(Thing.LUCKY_NUMBER);
     }
 }
 
