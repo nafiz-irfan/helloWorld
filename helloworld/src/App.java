4afdbc70388ed98d56b0cfcbf30473b0e8e06d0c
nafiz-irfan <nafiz.ace@gmail.com>
2019-09-24 22:59:32 +0800

    String Method

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,41 +1,29 @@
+class Frog{
+
+    private int id;
+    private String name;
+
+    public Frog(int id,String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public String toString(){
+//        return String.format("%-4d: %s", id, name);
+        StringBuilder sb = new StringBuilder();
+        sb.append(id).append(": ").append(name);
+        return sb.toString();
+    }
+
+}
+
 public class App {
     public static void main(String [] args){
+        Frog frog1 = new Frog(7, "Freddy");
+        Frog frog2 = new Frog(2, "Roger");
 
-        //Inefficient example
-        String info = "";
-
-        info += "My name is Bob.";
-        info += " ";
-        info += "I am a builder.";
-
-//        System.out.println(info);
-
-        //More efficient example
-        StringBuilder sb = new StringBuilder("");
-
-        sb.append("My name is Sue.");
-        sb.append(" ");
-        sb.append("I am a dentist.");
-
-//        System.out.println(sb.toString());
-
-        // The same as above, but nicer ....
-        StringBuilder s = new StringBuilder();
-
-        s.append("My name is Roger.").append(" ").append("I am an athlete.");
-
-//        System.out.println(s.toString());
-
-        ///// Formatting //////////////////////////////////
-
-        //Outputting newlines and tabs
-//        System.out.println("Here is some text.\tThat was a tab.\nThat was a newline.");
-//        System.out.println("More text.");
-
-        // Formatting integers
-        // %-10d means: output an integer in a space ten characters wide,
-        // padding with space and left-aligning (%10d would right-align)
-        System.out.printf("Total cost %-10d; quantity is %10d\n", 5 , 120);
+        System.out.println(frog1);
+        System.out.println(frog2);
     }
 }
 
