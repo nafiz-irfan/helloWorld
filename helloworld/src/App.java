e4a783d7cef98286d806b774a3242d4016f13c49
nafiz-irfan <nafiz.ace@gmail.com>
2019-09-24 23:29:03 +0800

    Packages

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,16 +1,11 @@
+import com.caveofprogramming.oceangame.*;
+import ocean.plants.*;
+import ocean.*;
+
 public class App {
     public static void main(String [] args){
-        Machine mach1 = new Machine();
-
-        mach1.start();
-        mach1.stop();
-
-        Car car1 = new Car();
-
-        car1.start();
-        car1.wipeWindShield();
-        car1.showInfo();
-        car1.stop();
+        Fish fish = new Fish();
+        Algae algae = new Algae();
     }
 }
 
