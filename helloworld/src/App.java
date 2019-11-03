3ad2ba3fc8666a68ded4c0939b13f9cc97d53837
nafiz-irfan <nafiz.ace@gmail.com>
2019-11-03 14:02:24 +0800

    Anonymous Classes

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,62 +1,37 @@
-import java.util.ArrayList;
-
 class Machine {
-    @Override
-    public String toString(){
-        return "I am a machine";
-    }
-
-    public String start(){
-        return "Machine starting";
+    public void start(){
+        System.out.println("Starting machine ...");
     }
 }
 
-class Camera extends Machine {
-    @Override
-    public String toString(){
-        return "I am a camera";
-    }
-
-    public String snap(){
-        return "snap!";
-    }
+interface Plant {
+    public void grow();
 }
 
 public class App {
-    public static void main(String [] args){
+    public static void main(String [] args) {
 
-        ArrayList<Machine> list1 = new ArrayList<Machine>();
-        list1.add(new Machine());
-        list1.add(new Machine());
+        // This is equivalent to creating a class that "extends"
+        // Machine and overrides the start method.
+        Machine machine1 = new Machine() {
+            @Override
+            public void start() {
+                System.out.println("Camera snapping ...");
+            }
+        };
 
+        machine1.start();
 
-        ArrayList<Camera> list2 = new ArrayList<Camera>();
+        // This is equivalent to creating a class that "implements"
+        // the Plant interface
+        Plant plant1 = new Plant() {
+            @Override
+            public void grow() {
+                System.out.println("Plant growing");
+            }
+        };
 
-        list2.add(new Camera());
-        list2.add(new Camera());
-
-        showList1(list2);
-        showList2(list1);
-        showList3(list1);
-    }
-
-    public static void showList1(ArrayList<? extends Machine> list){
-        for (Machine value : list){
-            System.out.println(value);
-            value.start();
-        }
-    }
-
-    public static void showList2(ArrayList<? super Camera> list){
-        for (Object value : list){
-            System.out.println(value);
-        }
-    }
-
-    public static void showList3(ArrayList<?> list){
-        for (Object value : list){
-            System.out.println(value);
-        }
+        plant1.grow();
     }
 }
 
