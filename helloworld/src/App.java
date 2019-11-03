9a6a7d600e8802b4af1fef6965db2e75ad4b92a5
nafiz-irfan <nafiz.ace@gmail.com>
2019-11-03 13:55:57 +0800

    Generics and Wildcards

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,44 +1,62 @@
 import java.util.ArrayList;
-import java.util.HashMap;
 
-class Animal { }
+class Machine {
+    @Override
+    public String toString(){
+        return "I am a machine";
+    }
+
+    public String start(){
+        return "Machine starting";
+    }
+}
+
+class Camera extends Machine {
+    @Override
+    public String toString(){
+        return "I am a camera";
+    }
+
+    public String snap(){
+        return "snap!";
+    }
+}
 
 public class App {
     public static void main(String [] args){
-        /////////////////// Before Java 5 ////////////////////////
-        ArrayList list = new ArrayList();
 
-        list.add("apple");
-        list.add("banana");
-        list.add("orange");
+        ArrayList<Machine> list1 = new ArrayList<Machine>();
+        list1.add(new Machine());
+        list1.add(new Machine());
 
-        String fruit = (String)list.get(1);
 
-        System.out.println(fruit);
+        ArrayList<Camera> list2 = new ArrayList<Camera>();
 
-        /////////////// Modern style //////////////////////////////
+        list2.add(new Camera());
+        list2.add(new Camera());
 
-        ArrayList<String> strings = new ArrayList<String>();
+        showList1(list2);
+        showList2(list1);
+        showList3(list1);
+    }
 
-        strings.add("cat");
-        strings.add("dog");
-        strings.add("duck");
+    public static void showList1(ArrayList<? extends Machine> list){
+        for (Machine value : list){
+            System.out.println(value);
+            value.start();
+        }
+    }
 
-        String animal = strings.get(1);
+    public static void showList2(ArrayList<? super Camera> list){
+        for (Object value : list){
+            System.out.println(value);
+        }
+    }
 
-        System.out.println(animal);
-
-        ///////////// There can be more than one type argument ////////////////////
-
-        HashMap<Integer, String>map = new HashMap<Integer, String>();
-
-        map.put(1,"test");
-        System.out.println(map);
-
-        //////////// Java 7 style /////////////////////////////////
-        ArrayList<Animal> someList = new ArrayList<>();
-        someList.add(new Animal());
-        System.out.println(someList);
-}
+    public static void showList3(ArrayList<?> list){
+        for (Object value : list){
+            System.out.println(value);
+        }
+    }
 }
 
