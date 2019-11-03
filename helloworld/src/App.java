9597bb68af10d62d86eda235b21acfb61d99e44c
nafiz-irfan <nafiz.ace@gmail.com>
2019-11-03 14:43:29 +0800

    Handling exceptions - Demo1

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,32 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.io.FileReader;
 
 public class App {
     public static void main(String [] args) throws FileNotFoundException {
 
-        String fileName = "example.txt"; //file should be place in root dir of the project
+        File file = new File("test.txt");
 
-        File textFile = new File(fileName);
-
-        Scanner in = new Scanner(textFile);
-
-        int value = in.nextInt();
-        System.out.println("Read Value: " + value);
-
-        if(in.hasNextLine()){
-        in.nextLine();
-        }
-
-        int count = 2;
-        while(in.hasNextLine()){
-            String line = in.nextLine();
-
-            System.out.println(count + ": " + line);
-            count++;
-        }
-
-        in.close();
+        FileReader fr = new FileReader(file);
     }
 }
 
