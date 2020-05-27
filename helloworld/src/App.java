021edfb6a74ad2da07c57601e370df0ffd72d303
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-27 22:48:16 +0800

    Recursion

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,30 +1,13 @@
 public class App {
     public static void main(String[] args) {
 
-        Animal animal = Animal.DOG;
+        System.out.println(factorial(5));
+    }
 
-        switch (animal) {
-            case CAT:
-            case MOUSE:
-            case DOG:
-                System.out.println(Animal.DOG.name());
-                break;
-            default:
-                throw new IllegalStateException("Unexpected value: " + animal);
-        }
+    private static int factorial(int val) {
+        if (val == 1) return 1;
 
-        System.out.println(Animal.DOG);
-        System.out.println("Enum name as a string: " + Animal.DOG.name());
-
-        System.out.println(Animal.DOG.getClass());
-
-        System.out.println(Animal.DOG instanceof Enum);
-
-        System.out.println(Animal.MOUSE.getName());
-
-        Animal animal1 = Animal.valueOf("CAT");
-
-        System.out.println(animal1);
+        return factorial(val - 1) * val;
     }
 }
 
