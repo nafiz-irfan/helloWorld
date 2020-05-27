799511be05beadd2f982a1abb0c04bddde861d31
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-27 21:25:33 +0800

    Reading file with File Reader

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,13 +1,37 @@
+import java.io.*;
+
 public class App {
     public static void main(String[] args) {
-        Camera camera = new Camera();
-        camera.setId(1);
+        File file = new File("E:/IdeaProjects/helloWorld/helloworld/src/test.txt");
 
-        Car car = new Car();
-        car.setId(2);
+        BufferedReader br = null;
 
-        car.run();
-        camera.run();
+        try {
+            FileReader fileReader = new FileReader(file);
+            br = new BufferedReader(fileReader);
+
+            String line;
+
+            while ((line = br.readLine()) != null){
+                System.out.println(line);
+            }
+
+        }catch (FileNotFoundException e){
+            System.out.println("File not found: " + file.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Unable to read file: " + file.toString());
+            e.printStackTrace();
+        }finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+            }catch (IOException e) {
+                System.out.println("Unable to close file: " + file.toString());
+                e.printStackTrace();
+            }catch (NullPointerException ex){}
+        }
     }
 }
 
