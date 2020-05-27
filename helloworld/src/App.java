23e1c15704ba119ff633e83fce020e649c8cd0f2
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-27 22:41:41 +0800

    Enumn Types - usage

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,11 +1,30 @@
 public class App {
     public static void main(String[] args) {
 
-        Robot robot = new Robot(1);
-        robot.start();
+        Animal animal = Animal.DOG;
 
-        Robot.Battery battery = new Robot.Battery();
-        battery.charge();
+        switch (animal) {
+            case CAT:
+            case MOUSE:
+            case DOG:
+                System.out.println(Animal.DOG.name());
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + animal);
+        }
+
+        System.out.println(Animal.DOG);
+        System.out.println("Enum name as a string: " + Animal.DOG.name());
+
+        System.out.println(Animal.DOG.getClass());
+
+        System.out.println(Animal.DOG instanceof Enum);
+
+        System.out.println(Animal.MOUSE.getName());
+
+        Animal animal1 = Animal.valueOf("CAT");
+
+        System.out.println(animal1);
     }
 }
 
