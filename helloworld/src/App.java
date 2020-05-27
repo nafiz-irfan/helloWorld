cb3caac87dcfe6b2bc7c4ddc9fe01ef131c1d9ed
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-27 22:14:16 +0800

    The equals() method

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,22 +1,59 @@
-import java.io.*;
+import java.util.Objects;
+
+class Person {
+    private int id;
+    private String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return id == person.id &&
+                Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+}
 
 public class App {
     public static void main(String[] args) {
 
-        File file = new File("test.txt");
+        System.out.println(new Object());
 
-        try (BufferedWriter br = new BufferedWriter(new FileWriter(file))){
-            br.write("This is first write to the file.\n");
-            br.write("This is second write to the file.");
+        Person person1 = new Person(1, "Abu");
+        Person person2 =  new Person(2,"Ali");
 
-            br.newLine();
-            br.write("This is third write to the file after new line.");
+        System.out.println(person1.equals(person2));
 
-        }catch (
-                IOException e) {
-            System.out.println("Unable to read file: " + file.toString());
-            e.printStackTrace();
-        }
+        Double value1 = 7.2;
+        Double value2 = 7.2;
+
+        System.out.println(value1.equals(value2));
+
+        String text1 = "Hello";
+        String text2 = "Helloasdf".substring(0,5);
+
+        System.out.println(text2);
+
+        System.out.println(text1.equals(text2));
     }
 
 }
