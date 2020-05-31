a9c5ae26c551fa1d72214502663897b76c0b3cc2
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-31 21:41:28 +0800

    Queues

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,69 +1,49 @@
-import java.util.*;
-
-class Person implements Comparable<Person> {
-    private String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Person{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person person = (Person) o;
-        return name.equals(person.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name);
-    }
-
-    @Override
-    public int compareTo(Person person) {
-        int len1 = name.length();
-        int len2 = person.name.length();
-
-        if (len1 > len2) return 1;
-        else if (len1 < len2) return -1;
-        else return name.compareTo(person.name);
-
-    }
-}
+import java.util.NoSuchElementException;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class App {
     public static void main(String[] args) {
 
-        List<Person> list = new ArrayList<>();
-        SortedSet<Person> set = new TreeSet<>();
+        Queue<Integer> q1 = new ArrayBlockingQueue<>(3);
 
-        addElements(list);
-        addElements(set);
+        q1.add(10);
+        q1.add(20);
+        q1.add(30);
 
-        Collections.sort(list);
+        System.out.println("Head of queue is: " + q1.element());
 
-        showElements(list);
-        System.out.println();
-        showElements(set);
-    }
+        try {
+            q1.add(40);
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        }
 
-    private static void addElements(Collection<Person> col) {
-        col.add(new Person("Joe"));
-        col.add(new Person("Sue"));
-        col.add(new Person("Juliet"));
-        col.add(new Person("Clare"));
-        col.add(new Person("Mike"));
-    }
+        for (Integer value : q1) System.out.println("Queue value: " + value);
 
-    private static void showElements(Collection<Person> col) {
-        for (Person element : col) System.out.println(element);
+        System.out.println("Removed from queue: " + q1.remove());
+        System.out.println("Removed from queue: " + q1.remove());
+        System.out.println("Removed from queue: " + q1.remove());
+
+        try {
+            System.out.println("Removed from queue: " + q1.remove());
+        } catch (NoSuchElementException e) {
+            e.printStackTrace();
+        }
+
+        Queue<Integer> q2 = new ArrayBlockingQueue<>(2);
+
+        System.out.println(q2.peek());
+
+        q2.offer(10);
+        q2.offer(20);
+
+        System.out.println(q2.peek());
+
+        if (q2.offer(30) == false) System.out.println("Offer failed to add third item");
+
+        for (Integer value : q2) System.out.println(value);
+
+        System.out.println("Queue 2 poll: " + q2.poll());
     }
 }
