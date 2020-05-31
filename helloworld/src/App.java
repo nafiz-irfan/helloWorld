238db827ca2e11a1f68594bbf507380160482b2b
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-31 21:46:02 +0800

    Using iterators

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,49 +1,29 @@
-import java.util.NoSuchElementException;
-import java.util.Queue;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) {
 
-        Queue<Integer> q1 = new ArrayBlockingQueue<>(3);
+        LinkedList<String> animals = new LinkedList<>();
 
-        q1.add(10);
-        q1.add(20);
-        q1.add(30);
+        animals.add("fox");
+        animals.add("cat");
+        animals.add("dog");
+        animals.add("rabbit");
 
-        System.out.println("Head of queue is: " + q1.element());
+        //old way
+        Iterator<String> it = animals.iterator();
 
-        try {
-            q1.add(40);
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
+        while (it.hasNext()) {
+            String value = it.next();
+            System.out.println(value);
+
+            if (value.equals("cat")) it.remove();
         }
 
-        for (Integer value : q1) System.out.println("Queue value: " + value);
+        System.out.println();
 
-        System.out.println("Removed from queue: " + q1.remove());
-        System.out.println("Removed from queue: " + q1.remove());
-        System.out.println("Removed from queue: " + q1.remove());
-
-        try {
-            System.out.println("Removed from queue: " + q1.remove());
-        } catch (NoSuchElementException e) {
-            e.printStackTrace();
-        }
-
-        Queue<Integer> q2 = new ArrayBlockingQueue<>(2);
-
-        System.out.println(q2.peek());
-
-        q2.offer(10);
-        q2.offer(20);
-
-        System.out.println(q2.peek());
-
-        if (q2.offer(30) == false) System.out.println("Offer failed to add third item");
-
-        for (Integer value : q2) System.out.println(value);
-
-        System.out.println("Queue 2 poll: " + q2.poll());
+        //modern iteration
+        for (String animal : animals) System.out.println(animal);
     }
 }
