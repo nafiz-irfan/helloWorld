4c1b7117649fe6a37211e936fa5202e07cab9d6a
nafiz-irfan <nafiz.ace@gmail.com>
2020-05-31 12:19:18 +0800

    Sorted Maps

--- helloworld/src/App.java
+++ helloworld/src/App.java
@@ -1,22 +1,27 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class App {
     public static void main(String[] args) {
 
-        HashMap<Integer, String> map = new HashMap<>();
+        Map<Integer, String> hashMap = new HashMap<>();
+        Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+        Map<Integer, String> treeMap = new TreeMap<>();
 
-        map.put(1, "One");
-        map.put(5, "Five");
-        map.put(2, "Two");
+        testMap(hashMap);
+    }
 
-        String text = map.get(1);
+    public static void testMap(Map<Integer, String> map) {
+        map.put(9, "fox");
+        map.put(4, "cat");
+        map.put(8, "dog");
+        map.put(1, "giraffe");
+        map.put(0, "swan");
 
-        System.out.println(text);
-
-        for (Map.Entry<Integer, String> entry : map.entrySet()) {
-            int key = entry.getKey();
-            String value = entry.getValue();
+        for (Integer key : map.keySet()) {
+            String value = map.get(key);
 
             System.out.println(key + ": " + value);
         }
